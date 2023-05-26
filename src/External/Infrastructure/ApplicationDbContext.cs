a7f7f3896977983d50cda847ba2c0e8c2b1b80d1
ngimdock <ngimdockzemfack@gmail.com>
2023-05-26 13:22:53 +0100

    add ApplicationDbContext

--- src/External/Infrastructure/ApplicationDbContext.cs
+++ src/External/Infrastructure/ApplicationDbContext.cs
@@ -1,5 +1,13 @@
+using Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 
 
-public sealed class ApplicationDbContext {
+public sealed class ApplicationDbContext: DbContext, IUnitOfWork {
+
+  public ApplicationDbContext(DbContextOptions options): base(options) 
+  {}
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder) => 
+    modelBuilder.ApplyConfigurationFromAssembly(typeof(ApplicationDbContext).Assembly);
 }
